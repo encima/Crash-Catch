aa92490788ebdd251b91b8687c0fcf0e7a9a5cbe
encima <encima@gmail.com>
2016-07-16 13:50:13 +0100

    added config for py file

---
+++ src/config.py
@@ -0,0 +1,3 @@
+url = "https://www.vehiclecheck.co.uk/?SC=132&vrm={}"
+plates = ["HV64 KFO"]
+#plates = ["H{}54 KPO","H{}54 KPD","H{}54 KFO","H{}54 KFD","H{}64 KPO","H{}64 KPD","H{}64 KFO","H{}64 KFD"]
