75694519dd2791ff6461b72ea0e4d556b6a45ffd
encima <encima@gmail.com>
2016-07-19 14:44:12 +0100

    changes for python3 compatibility

--- src/plate_check.py
+++ src/plate_check.py
@@ -6,19 +6,19 @@ import config
 
 
 matches = []
-letters = string.letters[:26]
+letters = string.ascii_letters[:26]
 f = open('../data/matches.txt', 'w')
 for plate in config.plates:
     for a in letters:
         for b in letters:
-			p = plate.format(a,b).lower()
-			print(p)
-			r = requests.get(config.url.format(p))
-			if r.status_code == 200 and "BMW" in r.text:
-				soup = BeautifulSoup(r.text, 'html.parser')
-				match = soup.find(id="searchResult")
-				print(match.h3)
-				matches.append({'plate':p, 'car': match})
-				f.writelines(match)
-			time.sleep(2)
+            p = plate.format(a,b).lower()
+            print(p)
+            r = requests.get(config.url.format(p))
+            if r.status_code == 200 and "BMW" in r.text:
+                soup = BeautifulSoup(r.text, 'html.parser')
+                match = soup.find(id="searchResult")
+                print(match.text)
+                matches.append({'plate':p, 'car': match})
+                f.writelines(match.text)
+            time.sleep(2)
 f.close()
